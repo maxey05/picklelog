49c3ca374783173ee48332550b0daa99e5e35b10
maxey <emgireyes17@gmail.com>
2026-09-23 22:08:01 +0800

    add domain module as pure Kotlin JVM module with no Android dependency

---
+++ domain/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.datetime)
+
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
+}
