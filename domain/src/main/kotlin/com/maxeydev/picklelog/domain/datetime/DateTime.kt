8dc517019620bf7a4e19f91f7f8a0eda80b0165f
maxey <emgireyes17@gmail.com>
2026-09-24 12:24:02 +0800

    typealias kotlinx-datetime types behind App types

---
+++ domain/src/main/kotlin/com/maxeydev/picklelog/domain/datetime/DateTime.kt
@@ -0,0 +1,7 @@
+package com.maxeydev.picklelog.domain.datetime
+
+typealias AppDate = kotlinx.datetime.LocalDate
+
+typealias AppTime = kotlinx.datetime.LocalTime
+
+typealias AppInstant = kotlin.time.Instant
