4f4006e3a870000f88caf2cb24b82ed9d6a95a1a
maxey <emgireyes17@gmail.com>
2026-09-24 13:07:42 +0800

    add Room type converters

---
+++ data/src/main/kotlin/com/maxeydev/picklelog/data/db/Converters.kt
@@ -0,0 +1,26 @@
+package com.maxeydev.picklelog.data.db
+
+import androidx.room.TypeConverter
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalTime
+import kotlin.time.Instant
+
+class Converters {
+    @TypeConverter
+    fun localDateToString(value: LocalDate?): String? = value?.toString()
+
+    @TypeConverter
+    fun stringToLocalDate(value: String?): LocalDate? = value?.let(LocalDate::parse)
+
+    @TypeConverter
+    fun localTimeToString(value: LocalTime?): String? = value?.toString()
+
+    @TypeConverter
+    fun stringToLocalTime(value: String?): LocalTime? = value?.let(LocalTime::parse)
+
+    @TypeConverter
+    fun instantToEpochMillis(value: Instant?): Long? = value?.toEpochMilliseconds()
+
+    @TypeConverter
+    fun epochMillisToInstant(value: Long?): Instant? = value?.let(Instant::fromEpochMilliseconds)
+}
